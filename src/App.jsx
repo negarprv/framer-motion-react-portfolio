350ddd1b08187ba4b3dd5ca2dfc5544ec062a491
Negar Parvaresh <negar.prv@gmail.com>
2024-03-02 22:08:19 +0330

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import "./app.scss";
+import Navbar from "./components/navbar/Navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section id="Portfolio">Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/sidebar/Links/Links.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const items = ["Homepage", "Services", "Portfolio", "Contact"];
+
+const variants = {
+  open: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    transition: {
+      staggerChildren: 0.05,
+      staggerDirection: -1,
+    },
+  },
+};
+
+const itemVarients = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+  },
+};
+
+const Links = () => {
+  return (
+    <motion.div className="links" variants={variants}>
+      {items.map((item) => (
+        <motion.a
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          variants={itemVarients}
+          href={`#${item}`}
+          key={item}
+        >
+          {item}
+        </motion.a>
+      ))}
+    </motion.div>
+  );
+};
+
+export default Links;
